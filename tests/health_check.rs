781c24b19387e2b1f38175050f1880f950f50a92
Kannan Palani <kannanpalani455@gmail.com>
2023-10-20 12:16:38 +0530

    health check integration test

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,18 +1,36 @@
+use std::net::TcpListener;
+
 
 #[tokio::test]
-fn dummy_test(){
-    spawn_app().await.expect("Failed to spawn our app")
+async fn health_check_works() {
+    
+    let address = spawn_app();
 
     let client = reqwest::Client::new();
 
-    let response = client.get("http://127.0.0.1:8000/health_check")
+    let response = client.get(&format!("{}/health_check", &address))
         .send()
-        .await
-        .expect("Failed to execute the request.")
+        .await 
+        .expect("Failed to execute request");
 
+    
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
 
+}
+
+fn spawn_app() -> String {
+
+    let listener = TcpListener::bind("127.0.0.1:0")
+        .expect("Failed to bind random port");
+
+    let port = listener.local_addr().unwrap().port();    
+
+    let server = newsletter::run(listener).expect("Failed to bind address");
+
+    let _ = tokio::spawn(server);       
+
+    format!("http://127.0.0.1:{}", port)
 }
 
 
