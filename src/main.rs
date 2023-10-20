968a82f986fc2ffec140f6ecb030deda74e1b24c
Kannan Palani <kannanpalani455@gmail.com>
2023-10-20 21:12:08 +0530

    add project structure

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use newsletter::run;
 use std::net::TcpListener;
+
+use newsletter::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:8000")
-    .expect("Failed to bind random port");
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
     run(listener)?.await
 }
